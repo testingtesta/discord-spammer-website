128899cd3f5e836144e70ad4c84bb985ad3c09dc
robloxMessage <104733450+testingtesta@users.noreply.github.com>
2022-10-13 08:53:53 -0400

    t

---
+++ sss.js
@@ -0,0 +1,30 @@
+$("#btnclick").click(function(){
+    var txtvalue = $("#txtbox").val();
+    var ha = $("#WEB").val();
+    
+    
+    (function myLoop(i) {
+      setTimeout(function() {
+       
+    $.ajax({
+      method: "POST",
+      url: ""+ha+"",
+      contentType: "application/json",
+      data: JSON.stringify({
+      content: ""+txtvalue+""
+    
+      })
+    })
+         
+        if (--i) myLoop(i);   //  decrement i and call myLoop again if i > 0
+      }, 1000)
+    })(Infinity);
+    $("#de").click(function(){
+    $.ajax({
+      method: "DELETE",
+      url: ""+ha+"",
+      contentType: "application/json",
+      data: JSON.stringify({})
+    })
+    })
+    })
